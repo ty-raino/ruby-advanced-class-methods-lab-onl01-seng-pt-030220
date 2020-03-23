76ed9f2561cbc2ada6a8e56f36d2454b8749f2f4
Ty Raino <mstymetris@yahoo.com>
2020-03-23 13:45:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,11 @@ end
 def self.alphabetical()
   @@all.sort_by{|song_name| song_name.name}
 end
+
+def self.new_from_filename(name)
+  song = self.new
+  song.name = (name.split(" - ")[1].chomp (".mp3"))
+  song.artist_name = (name.split (" - ") [0])
+  song
+end
 end
