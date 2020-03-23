d2fb4a6039392a21db5309523bee59b4efc80e48
Ty Raino <mstymetris@yahoo.com>
2020-03-23 13:48:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,42 +9,65 @@ class Song
   def save
     self.class.all << self
   end
+
   def self.create
     song = self.new
-    @@all << song
+    song.save
     song
   end
+
   def self.new_by_name(name)
     song = self.new
     song.name = name
     song
   end
+
   def self.create_by_name(name)
     song = self.create
     song.name = name
     song
   end
 
-def self.find_by_name(name)
-  @@all.find{|song_name| song_name.name == name}
-end
+  def self.find_by_name(name)
+    song = self.all.select { |song| song.name == name }
+    song[0]
+  end
 
-def self.find_or_create_by_name(name)
-  self.find_by_name(name) || self.create_by_name
-end
+  def self.find_or_create_by_name(name)
+    #binding.pry
+    if !find_by_name(name)
+      create_by_name(name)
+    else
+      find_by_name(name)
+    end
+  end
 
-def self.alphabetical()
-  @@all.sort_by{|song_name| song_name.name}
-end
+  def self.alphabetical
+    self.all.sort_by { |song| song.name[0]}
+  end
 
-def self.new_from_filename(name)
-  song = self.new
-  song.name = (name.split(" - ")[1].chomp (".mp3"))
-  song.artist_name = (name.split (" - ") [0])
-  song
-end
+  def self.new_from_filename(filename)
+    song_data = filename.sub!(/.mp3/,'').split(' - ')
 
-def self.destroy_all()
-  @@all.clear
-end
+    song = self.new
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
+
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_data = filename.sub!(/.mp3/,'').split(' - ')
+
+    song = self.create
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
+
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 end
